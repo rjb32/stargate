4b8f7e2b490ed400c5e01de6f08093533cc178f0
Remy Boutonnet <remy.boutonnet@turing.bio>
2026-01-24 15:15:31 +0100

    Add stargate clean command

--- tools/stargate/Stargate.cpp
+++ tools/stargate/Stargate.cpp
@@ -9,47 +9,17 @@
 #include "ProjectConfig.h"
 
 #include "FatalException.h"
+#include "FileUtils.h"
 
 using namespace stargate;
 using namespace argparse;
 
 constexpr const char* STARGATE_NAME = "stargate";
 
-int main(int argc, char** argv) {
+int runBuild(const std::string& configFilePath, const std::string& outDirPath,
+             bool isVerbose) {
     StargateConfig stargateConfig;
     ProjectConfig projectConfig;
-
-    // Parse arguments
-    ArgumentParser argParser(STARGATE_NAME);
-    std::string configFilePath;
-    std::string outDirPath;
-    bool isVerbose = false;
-
-    argParser.add_argument("-c", "-config")
-    .nargs(1)
-    .default_value("")
-    .metavar("stargate.yaml")
-    .help("Path to the config file")
-    .store_into(configFilePath);
-
-    argParser.add_argument("-o", "-out_dir")
-    .nargs(1)
-    .default_value("")
-    .metavar("stargate.out")
-    .help("Path to the output directory")
-    .store_into(outDirPath);
-
-    argParser.add_argument("--verbose")
-    .nargs(0)
-    .help("Set stargate into verbose mode")
-    .store_into(isVerbose);
-
-    try {
-        argParser.parse_args(argc, argv);
-    } catch (const std::runtime_error& err) {
-        spdlog::error("{}", err.what());
-        return EXIT_FAILURE;
-    }
 
     projectConfig.setVerbose(isVerbose);
 
@@ -73,4 +43,75 @@ int main(int argc, char** argv) {
     stargate.run(&projectConfig);
 
     return EXIT_SUCCESS;
+}
+
+int runClean(const std::string& outDirPath) {
+    StargateConfig stargateConfig;
+
+    if (!outDirPath.empty()) {
+        stargateConfig.setStargateDir(outDirPath);
+    }
+
+    const std::string& stargateDir = stargateConfig.getStargateDir();
+
+    if (!FileUtils::exists(stargateDir)) {
+        spdlog::info("Nothing to clean: {} does not exist", stargateDir);
+        return EXIT_SUCCESS;
+    }
+
+    FileUtils::removeDirectory(stargateDir);
+    spdlog::info("Cleaned: {}", stargateDir);
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char** argv) {
+    // Parse arguments
+    ArgumentParser argParser(STARGATE_NAME);
+    std::string configFilePath;
+    std::string outDirPath;
+    bool isVerbose = false;
+
+    argParser.add_argument("-c", "-config")
+        .nargs(1)
+        .default_value("")
+        .metavar("stargate.yaml")
+        .help("Path to the config file")
+        .store_into(configFilePath);
+
+    argParser.add_argument("-o", "-out_dir")
+        .nargs(1)
+        .default_value("")
+        .metavar("stargate.out")
+        .help("Path to the output directory")
+        .store_into(outDirPath);
+
+    argParser.add_argument("--verbose")
+        .nargs(0)
+        .help("Set stargate into verbose mode")
+        .store_into(isVerbose);
+
+    // Add clean subcommand
+    ArgumentParser cleanParser("clean");
+    cleanParser.add_description("Remove the stargate output directory");
+    std::string cleanOutDirPath;
+    cleanParser.add_argument("-o", "-out_dir")
+        .nargs(1)
+        .default_value("")
+        .metavar("sgc.out")
+        .help("Path to the output directory to clean")
+        .store_into(cleanOutDirPath);
+    argParser.add_subparser(cleanParser);
+
+    try {
+        argParser.parse_args(argc, argv);
+    } catch (const std::runtime_error& err) {
+        spdlog::error("{}", err.what());
+        return EXIT_FAILURE;
+    }
+
+    if (argParser.is_subcommand_used("clean")) {
+        return runClean(cleanOutDirPath);
+    }
+
+    return runBuild(configFilePath, outDirPath, isVerbose);
 }
